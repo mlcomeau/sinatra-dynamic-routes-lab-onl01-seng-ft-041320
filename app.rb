6e2869fe319d295c16bf011b91b82b9f88edafa6
Megan Comeau <mlcomeau99@gmail.com>
2020-05-26 20:55:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get '/reversename/:name' do 
+  get '/reversename/:name' do
     "#{params[:name].reverse}"
-  end 
+  end
 
 end
