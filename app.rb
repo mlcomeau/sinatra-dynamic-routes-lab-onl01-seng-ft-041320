268f91a1b9d8ad16cedff3d0c0244d9454c768a5
Megan Comeau <mlcomeau99@gmail.com>
2020-05-26 21:10:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,11 +9,14 @@ class App < Sinatra::Base
   get '/square/:number' do
     total = params[:number].to_i * params[:number].to_i
     "#{total}.to_s"
-  #  "#{params[:number].to_i ** 2}"
   end
 
   get '/say/:number/:phrase' do
-
+    repeat = ''
+    params[:number].to_i.times do
+      repeat += params[:phrase]
+    end
+    repeat
   end
 
 
